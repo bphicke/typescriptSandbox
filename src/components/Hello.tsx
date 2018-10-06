c4571b06eaa2a91a04f1d50751ea11eb2e3b832c
brian <bphicke@gmail.com>
2018-10-06 14:00:00 -0500

    Make stateful Hello component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,6 +4,7 @@ export interface Props {
     name: string;
     enthusiasmLevel?: number;
 }
+
 function Hello({ name, enthusiasmLevel = 1}: Props) {
     if (enthusiasmLevel <= 0) {
         throw new Error('You could be a little more enthusiastic. :D');
